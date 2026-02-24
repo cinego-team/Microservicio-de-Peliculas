07c662c064589bf30936a83a88734bba4ba4168c
Valentino <valentino.pajon@gmail.com>
2026-02-24 20:40:31 -0300

    Fix empleado, pelicula, ventas y media de funcion

--- axios_service/env.ts
+++ axios_service/env.ts
@@ -1,6 +1,6 @@
 export const config = {
     APIUsuariosUrls: {
         baseUrl: `http://localhost:3004`,
-        getDatosEmpleadoById: (id: number) => `/microservicio-usuarios/usuario/admin/datos-empleado/${id}`,
+        getDatosEmpleadoById: (id: number) => `/usuario/admin/datos-empleado/${id}`,
     },
 }
